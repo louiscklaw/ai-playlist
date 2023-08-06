90881928010d053742002a13ea64c20c29551ff4
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-06 22:21:09 +0800

    update format,

--- poe-tryout/docker-poe-tryout/src/openbox-firefox/src/worker/poe/googlePalm/index.js
+++ poe-tryout/docker-poe-tryout/src/openbox-firefox/src/worker/poe/googlePalm/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const puppeteer = require('puppeteer-extra');
 
 const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
