93c6ec2201fd0e18afdd6805a320edcadce4786c
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-06 21:48:08 +0800

    update tests/helloworld,

--- poe-tryout/docker-poe-tryout/src/openbox-firefox/src/tests/googlePalm/helloworld/test.sh
+++ poe-tryout/docker-poe-tryout/src/openbox-firefox/src/tests/googlePalm/helloworld/test.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-# curl -X GET http://localhost:3000/summary/test
+curl -X GET http://localhost:3000/googlePalm/helloworld
 
-curl -X POST http://localhost:3000/googlePalm/helloworld \
-   -H 'Content-Type: application/json' \
-   -d @./input.json \
-   -o result_input.json
+# curl -X POST http://localhost:3000/googlePalm/helloworld \
+#    -H 'Content-Type: application/json' \
+#    -d @./input.json \
+#    -o result_input.json
