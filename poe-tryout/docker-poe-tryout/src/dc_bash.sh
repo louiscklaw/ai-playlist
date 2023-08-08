b85bd1689cd662424fd6d979f02096bfe85d8093
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-08 12:26:46 +0800

    update relocate kue-scheduler-api to poe-scheduler-api,

--- poe-tryout/docker-poe-tryout/src/dc_bash.sh
+++ poe-tryout/docker-poe-tryout/src/dc_bash.sh
@@ -6,7 +6,7 @@ docker compose \
   -f ./docker-compose.yml \
   -f ./bait/docker-compose.yml \
   -f ./dbapi/docker-compose.yml \
-  -f ./kue-scheduler-api/docker-compose.yml \
+  -f ./poe-scheduler-api/docker-compose.yml \
   -f ./static-share/docker-compose.yml \
   -f ./openbox-firefox/docker-compose.yml \
   -f ./jobsdb-link-extractor/docker-compose.yml \
--- poe-tryout/docker-poe-tryout/src/dc_build.sh
+++ poe-tryout/docker-poe-tryout/src/dc_build.sh
@@ -6,7 +6,7 @@ docker compose \
   -f ./docker-compose.yml \
   -f ./bait/docker-compose.yml \
   -f ./dbapi/docker-compose.yml \
-  -f ./kue-scheduler-api/docker-compose.yml \
+  -f ./poe-scheduler-api/docker-compose.yml \
   -f ./static-share/docker-compose.yml \
   -f ./openbox-firefox/docker-compose.yml \
   -f ./changedetect/docker-compose.yml \
@@ -17,7 +17,7 @@ docker compose \
   -f ./docker-compose.yml \
   -f ./bait/docker-compose.yml \
   -f ./dbapi/docker-compose.yml \
-  -f ./kue-scheduler-api/docker-compose.yml \
+  -f ./poe-scheduler-api/docker-compose.yml \
   -f ./static-share/docker-compose.yml \
   -f ./openbox-firefox/docker-compose.yml \
   -f ./changedetect/docker-compose.yml \
