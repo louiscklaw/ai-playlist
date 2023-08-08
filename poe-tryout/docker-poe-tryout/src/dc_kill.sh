ae312486fec3f3ebf3b188191c4fa770adc08afa
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-08 09:28:36 +0800

    update docker compose command,

---
+++ poe-tryout/docker-poe-tryout/src/dc_kill.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker compose \
+  -f ./docker-compose.yml \
+  -f ./bait/docker-compose.yml \
+  -f ./dbapi/docker-compose.yml \
+  -f ./kue-scheduler-api/docker-compose.yml \
+  -f ./static-share/docker-compose.yml \
+  -f ./openbox-firefox/docker-compose.yml \
+  kill
