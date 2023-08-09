78cec7b627345090baee5a1ac270082c768252ce
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-09 13:26:20 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/poe-scheduler-api/tests/ask_jobsdb_post/test.js
+++ poe-tryout/docker-poe-tryout/src/poe-scheduler-api/tests/ask_jobsdb_post/test.js
@@ -1,18 +1,36 @@
 const fetch = require('node-fetch');
+var jobDescription =
+  'Responsibilities:\n\n\n\n\n\n\n\n Perform routine process validation sampling and testing activities with reference to approved protocol.\n\n\n\n\n\n\n\n Ensures the process validation sampling activities are conducted on a timely manner.\n\n\n\n\n\n\n\n Work closely with cross functional departments (i.e., Production/ QA/ QC).\n\n\n\n\n\n\n\n Assists in ad-hoc task assigned by supervisor.\nQualifications:\n\n\n\n\n\n\n\n DSE/ F.5 or above in Engineering/ Pharmaceutical/ Food/ Chemistry or related discipline.\n\n\n\n\n\n\n\n Knowledge of MS Office (Excel and Word).\n\n\n\n\n\n\n\n Preferably but not required to have knowledge of GMP and other regulatory requirements.\n\n\n\n\n\n\n\n Good command of spoken and written Chinese and English.\n\n\n\n\n\n\n\n Hardworking, cooperative, self-motivated and willing to learn.\n\n\n\n\n\n\n\n Willing to work overtime.\n \n \n \n \nFresh graduates are also welcomeWe offer attractive remuneration package including medical scheme, discretionary year-end bonus etc. Interested parties please send\nfull resume\nwith\nsalary expectation\nand\navailability\nby click "Apply Now". Only shortlisted candidates will be interviewed.Synco (H.K.) Ltd\nis an Equal Opportunities Employer. Personal data provided by job applicants will be used strictly in accordance with our personal data policy and for recruitment purposes only. We aim to respond to successful applicants within 8 weeks and related information will be kept in our file for up to 12 months for other suitable vacancies in our organization and thereafter destroyed.\n';
+
+// jobDescription = jobDescription.replace(/\n+/g, '\n');
+jobDescription = jobDescription.replace(/\n +/g, ' ');
+jobDescription = jobDescription.replace(/\n\n+/g, '\n');
 
 const body = {
   jobs_id: 'blablabla',
   job_post: 'we are employing a teacher',
-  preprompts: ['we are employing a teacher, understand ?'],
-  question_list: ['what is the post employing?'],
+  preprompts: [
+    'Hi, please help to understand the text below',
+    `title: Validation Assistant,
+company name: SYNCO (H.K.) Ltd,
+company address: Tai Po Area,
+post date: 4-Aug-2023,
+job highlight: DSE / F.5 or above, GMP, Tai Po,
+description: ${jobDescription}
+`,
+  ],
+  question_list: ['What is the title of the job ?'],
+  return_url: 'http://www.google.com',
 };
 
-Array(3)
+Array(1)
   .fill(0)
   .forEach(async (v, i) => {
     console.log(`posting ask ${i}...`);
 
-    const response = await fetch('http://localhost:3002/ask_jobsdb_post', {
+    const url = 'http://localhost:3002/ask_jobsdb_post';
+    // const url = 'http://localhost:3002/helloworld';
+    const response = await fetch(url, {
       method: 'post',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
--- poe-tryout/docker-poe-tryout/src/poe-scheduler-api/tests/ask_jobsdb_post/test.sh
+++ poe-tryout/docker-poe-tryout/src/poe-scheduler-api/tests/ask_jobsdb_post/test.sh
@@ -5,6 +5,7 @@ set -ex
 curl -X POST http://localhost:3002/ask_jobsdb_post \
    -H 'Content-Type: application/json' \
    -d @./input.json 
-  #  \
-  #  -o result_input.json
+   
+   # \
+   # -o result_input.json
 
