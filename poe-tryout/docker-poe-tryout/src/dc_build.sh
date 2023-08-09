d00094b5a04c3433e65a45caa42e13edff3136e6
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-10 03:28:56 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/dc_build.sh
+++ poe-tryout/docker-poe-tryout/src/dc_build.sh
@@ -2,6 +2,20 @@
 
 set -ex
 
+
+docker compose \
+  -f ./docker-compose.yml \
+  -f ./bait/docker-compose.yml \
+  -f ./dbapi/docker-compose.yml \
+  -f ./poe-scheduler-api/docker-compose.yml \
+  -f ./static-share/docker-compose.yml \
+  -f ./openbox-seat/docker-compose.yml \
+  -f ./changedetect/docker-compose.yml \
+  -f ./jobsdb-link-extractor/docker-compose.yml \
+  -f ./flow-handler/docker-compose.yml \
+  pull
+
+
 cd ./openbox-seat
   ./build_docker.sh
 cd -
