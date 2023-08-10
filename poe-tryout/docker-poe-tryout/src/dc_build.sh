4ff02a5346c3db14704503731b173d2ce7a0281e
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-10 11:22:50 +0800

    update docker build process,

--- poe-tryout/docker-poe-tryout/src/dc_build.sh
+++ poe-tryout/docker-poe-tryout/src/dc_build.sh
@@ -2,44 +2,33 @@
 
 set -ex
 
+YML_S='
+-f ./docker-compose.yml
+-f ./bait/docker-compose.yml
+-f ./dbapi/docker-compose.yml
+-f ./poe-scheduler-api/docker-compose.yml
+-f ./static-share/docker-compose.yml
+-f ./openbox-seat/docker-compose.yml
+-f ./changedetect/docker-compose.yml
+-f ./jobsdb-link-extractor/docker-compose.yml
+-f ./flow-handler/docker-compose.yml
+'
 
-docker compose \
-  -f ./docker-compose.yml \
-  -f ./bait/docker-compose.yml \
-  -f ./dbapi/docker-compose.yml \
-  -f ./poe-scheduler-api/docker-compose.yml \
-  -f ./static-share/docker-compose.yml \
-  -f ./openbox-seat/docker-compose.yml \
-  -f ./changedetect/docker-compose.yml \
-  -f ./jobsdb-link-extractor/docker-compose.yml \
-  -f ./flow-handler/docker-compose.yml \
-  pull
+YML_DEV_S="
+$YML_S
+-f ./docker-compose.dev.yml
+"
+
+docker compose $YML_DEV_S pull
 
 # NOTE: making of logickee/openbox-seat-ubuntu
 cd ./openbox-seat
   ./build_docker.sh
 cd -
 
-docker compose \
-  -f ./docker-compose.yml \
-  -f ./bait/docker-compose.yml \
-  -f ./dbapi/docker-compose.yml \
-  -f ./poe-scheduler-api/docker-compose.yml \
-  -f ./static-share/docker-compose.yml \
-  -f ./openbox-seat/docker-compose.yml \
-  -f ./changedetect/docker-compose.yml \
-  -f ./jobsdb-link-extractor/docker-compose.yml \
-  -f ./flow-handler/docker-compose.yml \
-  config > docker-compose.gen
+docker compose $YML_S config > docker-compose.gen
+docker compose $YML_DEV_S config > docker-compose.dev.gen
+docker compose $YML_DEV_S up -d --build
 
-docker compose \
-  -f ./docker-compose.yml \
-  -f ./bait/docker-compose.yml \
-  -f ./dbapi/docker-compose.yml \
-  -f ./poe-scheduler-api/docker-compose.yml \
-  -f ./static-share/docker-compose.yml \
-  -f ./openbox-seat/docker-compose.yml \
-  -f ./changedetect/docker-compose.yml \
-  -f ./jobsdb-link-extractor/docker-compose.yml \
-  -f ./flow-handler/docker-compose.yml \
-  up -d --build
+exit 0
+
