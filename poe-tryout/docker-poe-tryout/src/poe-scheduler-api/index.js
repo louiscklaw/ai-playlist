4f575ee9d206a0eb5ed5991b4723af8e265a069b
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-13 01:24:35 +0800

    update poe-scheduler-api,

--- poe-tryout/docker-poe-tryout/src/poe-scheduler-api/index.js
+++ poe-tryout/docker-poe-tryout/src/poe-scheduler-api/index.js
@@ -6,14 +6,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
 app.use(bodyParser.json());
 
 app.use('/ask_poe', require('./routes/ask_poe'));
 app.use('/process_new_job_post', require('./routes/process_new_job_post'));
 app.use('/ask_jobsdb_post', require('./routes/ask_jobsdb_post'));
 app.use('/done', require('./routes/done'));
-
 
 app.use('/postHelloworld', require('./routes/postHelloworld'));
 app.use('/helloworld', require('./routes/helloworld'));
@@ -22,6 +20,5 @@ app.use('/helloworld', require('./routes/helloworld'));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 
 // app.use('/postCbHelloworld', require('./routes/postCbHelloworld'));
