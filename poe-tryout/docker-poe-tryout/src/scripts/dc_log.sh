d8466111b70aae33ac28daab4acd1a2f54def1ea
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-16 16:07:46 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
@@ -4,7 +4,8 @@ set -ex
 
 docker compose \
   -f ./docker-compose.gen \
-  logs -f  diff-handler jobsdb-link-extractor
+  logs -f  
+  # diff-handler jobsdb-link-extractor
   
   #  bait jobsdb-link-extractor openbox-poe-seat1  flow-handler
   # bait jobsdb-link-extractor openbox-poe-seat1 poe-scheduler-api flow-handler
