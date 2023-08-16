2d271c7410d6abf67eef950ae466131ef256a359
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-16 13:59:56 +0800

    update display to json,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/tests/jobsdb_link_extract/index.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/tests/jobsdb_link_extract/index.js
@@ -1,27 +1,8 @@
 const fetch = require('node-fetch');
 
-// const SAMPLE_PREPROMPTS = [
-//   'Forget everything and start a new talk.',
-// `
-// I will input a job advertisement to you,
-// can you help me to summarize it ?
-// Please try to make it in less than 100 words.
-// `.trim(),
-// `
-// Vickmans Laboratories Ltd
-// Validation Engineer(QA)
-// Ngau Tau Kok
-// Posted on 11-Aug-23
-// Job Highlights
-// `.trim()
-// ]
-
-// const SAMPLE_QUESTIONS = [
-//   'please help to draft a email describing where do you live. in less than 50 words'
-// ]
-
 // jobsdb_job_url:'http://bait:8080/jobsdb/hk/en/job/validation-assistant-100003010509868.html',
 // jobsdb_job_url:'https://hk.jobsdb.com/hk/en/job/validation-assistant-100003010509868',
+
 const payload = {
   jobsdb_job_url: `https://hk.jobsdb.com/hk/en/job/qa-engineer-automation-100003010496802`,
   callback_url: 'http://flow-handler:3000/jobsdb_link_extract_cb',
@@ -40,7 +21,7 @@ Array(1)
       headers: { 'Content-Type': 'application/json' },
     });
 
-    console.log(await response.text());
+    console.log(await response.json());
 
     // const res_json = await response.json();
     // console.log({ res_json });
