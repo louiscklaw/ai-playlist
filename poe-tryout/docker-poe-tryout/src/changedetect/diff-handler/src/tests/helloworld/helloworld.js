5e4f3feec9649b7c71709ed73802f9c9936b6dec
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-16 16:07:52 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/helloworld/helloworld.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/helloworld/helloworld.js
@@ -3,10 +3,18 @@ const fetch = require('node-fetch');
 const body = {
   "version":"1.0",
   "title":"JOBSDB_FOUND_NEW_JOB-http://bait:8080/jobsdb/hk/en/job/list_jobsdb.html",
-  "message":"(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402\n(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532404\n",
+  "message":"(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402",
   "attachments":[],
   "type":"info"
 }
+
+// const body = {
+//   "version":"1.0",
+//   "title":"JOBSDB_FOUND_NEW_JOB-http://bait:8080/jobsdb/hk/en/job/list_jobsdb.html",
+//   "message":"(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402\n(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532404\n",
+//   "attachments":[],
+//   "type":"info"
+// }
 
 Array(1)
   .fill(0)
