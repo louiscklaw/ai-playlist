1dca365f0b4f4cdaaf2d98c720771c8592088100
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-16 16:06:39 +0800

    update test url,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/tests/jobsdb_link_extract/index.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/tests/jobsdb_link_extract/index.js
@@ -3,8 +3,12 @@ const fetch = require('node-fetch');
 // jobsdb_job_url:'http://bait:8080/jobsdb/hk/en/job/validation-assistant-100003010509868.html',
 // jobsdb_job_url:'https://hk.jobsdb.com/hk/en/job/validation-assistant-100003010509868',
 
+// jobsdb_job_url: `https://hk.jobsdb.com/hk/en/job/qa-engineer-automation-100003010496802`,
+// jobsdb_job_url: `https://hk.jobsdb.com/hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402`,
+
+// /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402
 const payload = {
-  jobsdb_job_url: `https://hk.jobsdb.com/hk/en/job/qa-engineer-automation-100003010496802`,
+  jobsdb_job_url: `https://hk.jobsdb.com/hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402`,
   callback_url: 'http://flow-handler:3000/jobsdb_link_extract_cb',
 };
 
