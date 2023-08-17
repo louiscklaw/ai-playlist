2a93655ee380e9992f576bd8231cd9d8cc4271ce
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-17 18:17:00 +0800

    update,

---
+++ poe-tryout/docker-poe-tryout/src/volumes/openbox-poe-seat2/renew_user_data.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo $PWD
+
+# rm -rf firefox-user-data
+# cp -r /workspace/ai-playlist/poe-tryout/docker-poe-tryout/src/openbox-firefox/share.del/firefox-user-data \
+#   .
+
+docker kill openbox-poe-seat2
+
+rm -rf ./chrome-user-data
+cp -r ../../../_seeds/chrome-user-data-seed/ ./chrome-user-data
+
+rm -rf ./firefox-user-data
+cp -r ../../../_seeds/firefox-user-data-seed/ ./firefox-user-data
+
+docker start openbox-poe-seat2
