37e6468e3163f46b9059a0fe75a9cdbebd030b65
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-17 11:04:30 +0800

    update getAddedLink and its test to handle ?,

--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/routes/getAddedLink.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/routes/getAddedLink.js
@@ -2,7 +2,8 @@ function getAddedLink(messages){
   const temp = messages
     .filter(m => m != '')
     .filter(s => s.search(/(into|added)/g) > -1)
-    .map(m => m.replace(/\((into|added)\) /g,'').trim())
+    .map(m => m.replace(/\((into|added)\) /g,''))
+    .map(m => m.split('?')[0].trim())
 
   return temp
 }
--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/jobsdb_diff_handler/helloworld.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/jobsdb_diff_handler/helloworld.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const body = {
   "version":"1.0",
   "title":"JOBSDB_FOUND_NEW_JOB-http://bait:8080/jobsdb/hk/en/job/list_jobsdb.html",
-  "message":"(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402",
+  "message":"(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402?token=0~c7001e72-05f1-43f0-a9e0-ed4ded06b90f§ionRank=25&jobId=jobsdb-hk-job-100003010532402",
   "attachments":[],
   "type":"info"
 }
