ecfaaadbaed3d93480d72683e789998515de8a59
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-17 16:43:03 +0800

    update docker-poe-tryout,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/routes/reportJobComplete.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/routes/reportJobComplete.js
@@ -9,12 +9,14 @@ router.post('/', async (req, res) => {
 
   try {
     var req_body = req.body;
-    output = { ...output, state: 'success', debug: req_body };
+    output = { ...output, state: 'start', debug: req_body };
 
     const response = await postTelegramMessage('helloworld-text-blablabla');
     const res_json = await response.json();
 
     if (res_json.state != 'SEND_MESSAGE_DONE') throw new Error('error reported from page-handler endpoint');
+
+    output = { ...output, state: 'done' };
   } catch (error) {
     console.log(error);
     output = { ...output, state: 'error', error: error.message };
