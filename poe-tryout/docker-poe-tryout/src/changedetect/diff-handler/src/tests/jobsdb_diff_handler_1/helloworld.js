03b1cc8cad8f875f88b071b3458ff534f16b257b
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-18 02:07:34 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/jobsdb_diff_handler_1/helloworld.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/tests/jobsdb_diff_handler_1/helloworld.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
-const string_links = require('./string_links')
 
 const body = {
   version: '1.0',
   title: 'JOBSDB_FOUND_NEW_JOB-http://bait:8080/jobsdb/hk/en/job/list_jobsdb.html',
-  message: string_links,
+  message:
+    '(added)       /hk/en/job/actuarial-intern-valuation-2024-jan-jun-100003010532402?token=0~c7001e72-05f1-43f0-a9e0-ed4ded06b90f§ionRank=25&jobId=jobsdb-hk-job-100003010532402',
   attachments: [],
   type: 'info',
 };
@@ -31,5 +31,3 @@ Array(1)
     const res_json = await response.json();
     console.log({ res_json });
   });
-
-
