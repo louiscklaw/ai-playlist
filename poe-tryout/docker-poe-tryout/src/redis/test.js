3589f9b9548c1880b12fc7d82188341fc769d907
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-18 22:38:55 +0800

    update,

---
+++ poe-tryout/docker-poe-tryout/src/redis/test.js
@@ -0,0 +1,37 @@
+var redis = require('redis'); // 1
+
+const client = redis.createClient({ host: 'redis' });
+
+client.on('connect', () => {
+  console.log('Connected to Redis');
+
+  // Perform operations on Redis here
+
+  Array(999)
+    .fill(0)
+    .forEach((_,i) => {
+      console.log(i)
+      client.set('message', 'Hello World', (error, result) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`Set message: ${result}`);
+
+          client.get('message', (error, reply) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log(`Get message: ${reply}`);
+
+              // Close the connection when done with operations
+              client.quit();
+            }
+          });
+        }
+      });
+    });
+});
+
+client.on('error', error => {
+  console.error(`Error connecting to Redis: ${error}`);
+});
