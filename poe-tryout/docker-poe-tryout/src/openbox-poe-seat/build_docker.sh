9dd18dc3621ee94c88385e70923a0382442a767c
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-19 15:51:09 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/openbox-poe-seat/build_docker.sh
+++ poe-tryout/docker-poe-tryout/src/openbox-poe-seat/build_docker.sh
@@ -2,54 +2,56 @@
 
 set -x
 
-docker rmi openbox-poe-seat
+# TODO: resume me
+# docker image rm -f logickee/openbox-poe-seat-base
+docker image rm -f logickee/openbox-poe-seat-apps
+docker image rm -f logickee/openbox-poe-seat-final
+# docker image rm -f logickee/openbox-poe-seat
 
-set -ex
+docker pull ubuntu:22.04
+docker image tag ubuntu:22.04 logickee/openbox-poe-seat-base
 
 # build openbox as base
 cd dockerfiles/ubuntu
-  docker build . -t openbox-poe-seat
+  docker build . -t logickee/openbox-poe-seat-base
 cd -
 
 # build openbox as base
 cd dockerfiles/openbox
-  docker build . -t openbox-poe-seat
+  docker build . -t logickee/openbox-poe-seat-base
 cd -
 
+# NOTE: base end here
+
 cd dockerfiles/apps
-  docker build -f dockerfile.puppeteer . -t openbox-poe-seat
-  docker build -f dockerfile.vnc . -t openbox-poe-seat
-  docker build -f dockerfile.firefox . -t openbox-poe-seat
-  docker build -f dockerfile.chrome . -t openbox-poe-seat
+  docker image tag logickee/openbox-poe-seat-base logickee/openbox-poe-seat-apps
+  docker build -f dockerfile.puppeteer . -t logickee/openbox-poe-seat-apps
+  docker build -f dockerfile.vnc . -t logickee/openbox-poe-seat-apps
+  docker build -f dockerfile.firefox . -t logickee/openbox-poe-seat-apps
+  docker build -f dockerfile.chrome . -t logickee/openbox-poe-seat-apps
 cd -
 
 cd dockerfiles/apps/mitmproxy
-  docker build . -t openbox-poe-seat
+  docker build . -t logickee/openbox-poe-seat-apps
 cd -
+
+# NOTE: apps end here
 
 # finialize docker
 cd dockerfiles/final
+  # NOTE: do nothing but just init transfer tag at the very beginning
+  docker image tag logickee/openbox-poe-seat-apps logickee/openbox-poe-seat-final
+
   docker build . \
     --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
-    -t openbox-poe-seat
+    -t logickee/openbox-poe-seat-final
+
+  # TODO: tag openbox-poe-seat-final here
 cd -
 
-# docker image tag openbox-poe-seat logickee/openbox-poe-seat
-# docker push logickee/openbox-poe-seat
 
-# docker run --rm -it \
-#   --privileged \
-#   --device /dev/kvm \
-#   -p 6080:6080 \
-#   -p 4723:4723 \
-#   openbox-poe-seat \
-#   bash
+# docker image tag logickee/openbox-poe-seat-apps logickee/openbox-poe-seat-final
 
-  # -v ./share:/share \
-  # -v /root/.config:/share/.config:ro \
-  # -p 15900:5900 \
-#   # bash
-#   # bash
-
-# docker run -d -p 127.0.0.1:5901:5901 fullaxx/ubuntu-desktop
-# docker compose up -d
+docker push logickee/openbox-poe-seat-base
+docker push logickee/openbox-poe-seat-apps
+docker push logickee/openbox-poe-seat-final
