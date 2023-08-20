161dfc2cc0e4b78b30a4fed132861592c74757fd
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-20 15:43:11 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_dev.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_dev.sh
@@ -2,56 +2,14 @@
 
 set -e
 
-YML_S='
--f ./docker-compose.yml
--f ./api-debug/docker-compose.yml
--f ./bait/docker-compose.yml
--f ./changedetect/docker-compose.yml
--f ./dbapi/docker-compose.yml
--f ./flow-handler/docker-compose.yml
--f ./jobsdb-link-extractor/docker-compose.yml
--f ./openbox-poe-seat/docker-compose.yml
--f ./poe-scheduler-api/docker-compose.yml
--f ./static-share/docker-compose.yml
--f ./page-handler/docker-compose.yml
--f ./v2raya/docker-compose.yml
-'
+./scripts/dc_gen.sh
 
-if [[ -v DOCKER_DEV ]]; then
-  echo 
-  echo 
-  echo -e "\033[31m USING DEV DOCKER COMPOSE CONFIG !!! \033[0m"
-  echo 
-  echo 
-  read -p "Press Enter to continue..."
+echo 
+echo -e "\033[31m re-up docker compose ? \033[0m"
+read -p "Press Enter to continue..."
+echo 
 
-  YML_S="$YML_S -f ./docker-compose.dev.yml"
-  
-else
-  echo 
-  echo 
-  echo -e "\033[31m USING PROD DOCKER COMPOSE CONFIG !!! \033[0m"
-  echo 
-  echo 
-  read -p "Press Enter to continue..."
-fi
-
-set -ex
-
-# echo -e "\033[31m docker environment cleared !!! \033[0m"
-# read -p "Press Enter to continue..."
-
-# docker compose $YML_S pull
-
-# docker compose $YML_S kill
-# docker compose $YML_S down
-# sleep 1
-
-# docker volume rm -f src_redis_data
-# docker volume rm -f src_redisinsight_store
-
-docker compose $YML_S config > docker-compose.gen
-docker compose $YML_S up -d 
+docker compose -f ./docker-compose.gen up -d --remove-orphans
 # --build
 
 exit 0
---
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_gen.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+YML_S='
+-f ./docker-compose.yml
+-f ./api-debug/docker-compose.yml
+-f ./bait/docker-compose.yml
+-f ./changedetect/docker-compose.yml
+-f ./dbapi/docker-compose.yml
+-f ./flow-handler/docker-compose.yml
+-f ./jobsdb-link-extractor/docker-compose.yml
+-f ./openbox-poe-seat/docker-compose.yml
+-f ./poe-scheduler-api/docker-compose.yml
+-f ./static-share/docker-compose.yml
+-f ./page-handler/docker-compose.yml
+-f ./v2raya/docker-compose.yml
+'
+
+if [[ -v DOCKER_DEV ]]; then
+  echo 
+  echo -e "\033[31m USING DEV DOCKER COMPOSE CONFIG !!! \033[0m"
+  echo 
+  read -p "Press Enter to continue..."
+
+  YML_S="$YML_S -f ./docker-compose.dev.yml"
+  
+else
+  echo 
+  echo -e "\033[31m USING PROD DOCKER COMPOSE CONFIG !!! \033[0m"
+  echo 
+  read -p "Press Enter to continue..."
+fi
+
+docker compose $YML_S config > docker-compose.gen
