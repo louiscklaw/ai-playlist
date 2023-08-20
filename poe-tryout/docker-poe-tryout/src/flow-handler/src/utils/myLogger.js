fe040fd129cb1fc54cd923a51d6910f590c1d5e4
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-21 02:53:24 +0800

    update myLogger,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/utils/myLogger.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/utils/myLogger.js
@@ -1,27 +1,38 @@
 const winston = require('winston');
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, prettyPrint } = format;
+const { combine, timestamp, label, prettyPrint, printf, splat } = format;
+const path = require('path');
 
-const logger = winston.createLogger({
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  let msg = `${timestamp} [${level}] : ${message} `;
+  if (metadata) {
+    msg += JSON.stringify(metadata);
+  }
+  return msg;
+});
+
+const myLogger = winston.createLogger({
   level: 'info',
   format: combine(
-    winston.format.json(),
-    timestamp(),
-    // prettyPrint()
+    format.colorize(),
+    splat(),
+    label({ label: 'api-debug' }),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    myFormat,
   ),
-  defaultMeta: { service: 'flow-handler' },
   transports: [
-    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: './logs/combined.log' }),
+    new transports.Console({ level: 'info' }),
+    new winston.transports.File({ filename: '/logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: '/logs/combined.log' }),
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  );
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(
+//     new winston.transports.Console({
+//       format: winston.format.simple(),
+//     }),
+//   );
+// }
 
-module.exports = { myLogger: logger };
+module.exports = { myLogger };
