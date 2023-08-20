6dd76ac27c213495fb52b3841a67414842741a90
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-20 14:58:13 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_pull.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_pull.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
-docker pull logickee/poe-tryout-bait                    
-docker pull logickee/poe-tryout-dbapi                   
-docker pull logickee/poe-tryout-flow-handler            
-docker pull logickee/poe-tryout-jobsdb-link-extractor   
-docker pull logickee/poe-tryout-page-handler            
-docker pull logickee/poe-tryout-poe-scheduler-api       
-docker pull logickee/poe-tryout-static-share            
+docker pull logickee/poe-tryout-bait                    &
+docker pull logickee/poe-tryout-dbapi                   &
+docker pull logickee/poe-tryout-flow-handler            &
+docker pull logickee/poe-tryout-jobsdb-link-extractor   &
+docker pull logickee/poe-tryout-page-handler            &
+docker pull logickee/poe-tryout-poe-scheduler-api       &
+docker pull logickee/poe-tryout-static-share            &
+wait
