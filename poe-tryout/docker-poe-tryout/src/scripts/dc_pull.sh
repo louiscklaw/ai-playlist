96e3afdc1f917c044279e37f577a36b186f21951
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-20 15:01:50 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_pull.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_pull.sh
@@ -2,11 +2,25 @@
 
 set -ex
 
-docker pull logickee/poe-tryout-bait                    &
-docker pull logickee/poe-tryout-dbapi                   &
-docker pull logickee/poe-tryout-flow-handler            &
-docker pull logickee/poe-tryout-jobsdb-link-extractor   &
-docker pull logickee/poe-tryout-page-handler            &
-docker pull logickee/poe-tryout-poe-scheduler-api       &
-docker pull logickee/poe-tryout-static-share            &
-wait
+docker pull browserless/chrome                        &          
+docker pull ghcr.io/dgtlmoon/changedetection.io       &                       
+docker pull mongo-express:latest                      &        
+docker pull mongo:latest                              &
+docker pull mzz2017/v2raya                            &  
+docker pull node:latest                               &
+docker pull redis:6.2-alpine                          &    
+docker pull redislabs/redisinsight:latest             &    
+             
+docker pull logickee/openbox-poe-seat-final           &                   
+docker pull logickee/poe-tryout-bait                  &            
+docker pull logickee/poe-tryout-dbapi                 &             
+docker pull logickee/poe-tryout-diff-handler          &                    
+docker pull logickee/poe-tryout-flow-handler          &                    
+docker pull logickee/poe-tryout-jobsdb-link-extractor &                             
+docker pull logickee/poe-tryout-jobsdb-scraper        &                      
+docker pull logickee/poe-tryout-page-handler          &                    
+docker pull logickee/poe-tryout-poe-scheduler-api     &                         
+docker pull logickee/poe-tryout-static-share          &                    
+
+wait 
+
