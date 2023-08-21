138afe2fa7571b19caa03c59ebc3a3c6748f3e8a
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-21 11:19:14 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/utils/postResult.js
+++ poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/utils/postResult.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fetch = require('node-fetch');
+const {myLogger} = require('./myLogger');
 
 function fetchPost(url, json_body) {
   myLogger.info('fetchPost ' + url + ' ... ');
