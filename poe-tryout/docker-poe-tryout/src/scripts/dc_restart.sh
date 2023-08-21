52c1781f3650dc70b3a317bebb186397775589e7
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-21 17:59:14 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_restart.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_restart.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # docker compose -f ./docker-compose.gen.yml kill redis poe-scheduler-api
-docker compose -f ./docker-compose.gen.yml kill poe-scheduler-api
+docker compose -f ./docker-compose.gen.yml kill
 
 # docker volume rm src_redis_data
-docker compose -f ./docker-compose.gen.yml up -d --build poe-scheduler-api
+docker compose -f ./docker-compose.gen.yml up -d --build
