12f403579300486069e8c072c0b566c834110bd0
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-22 14:43:13 +0800

    update log filter

--- poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
@@ -3,25 +3,4 @@
 set -ex
 
 docker compose -f ./docker-compose.gen.yml \
-  logs -f 
-  # flow-handler jobsdb-link-extractor
-  # poe-scheduler-api
-  # redis 
-  # redisinsight
-  # poe-scheduler-api jobsdb-link-extractor diff-handler flow-handler dbapi
-  # jobsdb-scraper bait diff-handler
-  # poe-scheduler-api redis
-  # diff-handler flow-handler
-  #  diff-handler page-handler
-  # jobsdb-link-extractor
-  # redis
-  # diff-handler
-  # bait
-
-  # 
-  # diff-handler jobsdb-link-extractor
-  #  bait jobsdb-link-extractor openbox-poe-seat1  flow-handler
-  # bait jobsdb-link-extractor openbox-poe-seat1 poe-scheduler-api flow-handler
-  # flow-handler 
-  # -f jobsdb-link-extractor
-  # poe-scheduler-api
+  logs -f  api-debug dbapi diff-handler flow-handler jobsdb-link-extractor jobsdb-scraper page-handler poe-scheduler-api
