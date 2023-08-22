52251303f5bb59cdf66dd34ad6e7beb704d6fa12
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-22 13:26:00 +0800

    update,

---
+++ poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/utils/getFromEvaluateTextContent.js
@@ -0,0 +1,32 @@
+const { myLogger } = require('./myLogger');
+
+async function getFromEvaluateTextContent(jobPage, title_selector) {
+  var output = { result: '', error: {} };
+
+  try {
+    const o_jobTitle = await jobPage.evaluate(selector => {
+      var output = { result: '', error: {} };
+      try {
+        const title = document.querySelector(selector).textContent;
+        output = { ...output, result: title };
+      } catch (error) {
+        output = { ...output, error: error.message };
+      }
+      return output;
+    }, title_selector);
+
+    output = { ...output, result: o_jobTitle.result };
+    // if (o_jobTitle.result == '')
+    throw new Error('getFromEvaluateTextContent found empty');
+  } catch (error) {
+    myLogger.error('getFromEvaluateTextContent found empty');
+    myLogger.error('"%o', {
+      title_selector,
+      url: jobPage.url(),
+    });
+    output = { ...output, error: error.message };
+  }
+  return output;
+}
+
+module.exports = { getFromEvaluateTextContent };
