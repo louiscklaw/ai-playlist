89b3e13d4a87f02e2b2aed4cf8c09c0791f85c57
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-22 13:52:43 +0800

    update to use myLogger,

--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/routes/jobsdb_diff_handler.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/routes/jobsdb_diff_handler.js
@@ -3,7 +3,7 @@ const { postJobsdbLinkExtract } = require('../utils/postJobsdbLinkExtract');
 
 const express = require('express');
 const router = express.Router();
-const {myLogger} = require('../utils/myLogger')
+const { myLogger } = require('../utils/myLogger');
 
 const { getAddedLink } = require('../utils/getAddedLink');
 
@@ -16,8 +16,8 @@ function getPayloadToFlowHandlerJson(diff_link) {
       callback_url: 'http://flow-handler:3000/jobsdb_link_extract_cb',
     };
   } catch (error) {
-    console.log(error);
-    console.log({ diff_link });
+    myLogger.error(error.message);
+    myLogger.error("%o",{ diff_link });
   }
 }
 
@@ -28,13 +28,13 @@ router.post('/dump', (req, res) => {
     var req_body = req.body;
 
     // TODO: use myLogger,
-    console.log('dump called');
-    console.log({ req_body });
+    myLogger.info('dump called');
+    myLogger.info("%o",{ req_body });
 
     output = { ...output, state: 'done', debug: req_body };
   } catch (error) {
-    console.log('error occur in diff-handler');
-    console.log(error);
+    myLogger.error('error occur in diff-handler');
+    myLogger.error("%o",error);
     output = { ...output, state: 'error', error: error.message };
   }
 
@@ -50,7 +50,7 @@ router.post('/', (req, res) => {
     output = { ...output, state: 'start', debug: req_body };
 
     myLogger.info('call to jobsdb_diff_handler');
-    myLogger.info("%o",{ req_body });
+    myLogger.info('%o', { req_body });
 
     const json_message = req_body.message;
     const messages = json_message.split(/\n/);
@@ -59,25 +59,25 @@ router.post('/', (req, res) => {
     const flow_handler_payloads = sainted_messages.map(m => {
       return getPayloadToFlowHandlerJson(m);
     });
-    myLogger.info("%o",{ flow_handler_payloads });
+    myLogger.info('%o', { flow_handler_payloads });
 
     flow_handler_payloads.forEach(async pl => {
       try {
-        myLogger.info(`going to send postJobsdbLinkExtract -> `)
-        console.log(pl);
+        myLogger.info(`going to send postJobsdbLinkExtract -> `);
+        myLogger.info("%o",pl);
 
         await postJobsdbLinkExtract(pl);
       } catch (error) {
-        console.log(error);
-        console.log(pl);
+        myLogger.error("%o",error);
+        myLogger.error("%o",pl);
         throw new Error(`error during posting to flow-handler`);
       }
     });
 
     output = { ...output, state: 'done' };
   } catch (error) {
-    console.log('error occur in diff-handler');
-    console.log(error);
+    myLogger.error('error occur in diff-handler');
+    myLogger.error("%o",error);
     output = { ...output, state: 'error', error: error.message };
   }
 
