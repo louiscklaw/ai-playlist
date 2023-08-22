2f4bc8abf9457a4a4fd250a0c17137bd77d8295d
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-22 13:24:26 +0800

    update selector to handle empty,

--- poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/routes/jobsdbPostExtract.js
+++ poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/routes/jobsdbPostExtract.js
@@ -20,34 +20,37 @@ router.post('/', async (req, res) => {
   try {
     // NOTE: sender-> src/flow-handler/src/state_machine/jobsdb/onExtractJobDetail.js
     const { jobsdb_job_url, callback_url } = req.body;
-    if (!jobsdb_job_url) throw new Error('jobsdb job url is undefined')
+    if (!jobsdb_job_url) throw new Error('jobsdb job url is undefined');
 
     res.send({ state: 'scheduled' });
 
-    async function getFromEvaluate(jobPage, title_selector) {
-      var output = { result: '', error: {} }
+    async function getFromEvaluateTextContent(jobPage, title_selector) {
+      var output = { result: '', error: {} };
 
-      const o_jobTitle = await jobPage.evaluate((selector) => {
-        var output = { result: '', error: {} }
-        try {
-          const title = document.querySelector(selector).textContent;
-          output = { ...output, result: title }
-        } catch (error) {
-          output = { ...output, error: error.message }
-        }
-        return output;
-      }, title_selector);
+      try {
+        const o_jobTitle = await jobPage.evaluate(selector => {
+          var output = { result: '', error: {} };
+          try {
+            const title = document.querySelector(selector).textContent;
+            output = { ...output, result: title };
+          } catch (error) {
+            output = { ...output, error: error.message };
+          }
+          return output;
+        }, title_selector);
 
-      output = { ...output, result: o_jobTitle.result }
-      if (o_jobTitle.result == '') {
-        myLogger.error('getFromEvaluate found empty')
+        output = { ...output, result: o_jobTitle.result };
+        // if (o_jobTitle.result == '')
+        throw new Error('getFromEvaluateTextContent found empty');
+      } catch (error) {
+        myLogger.error('getFromEvaluateTextContent found empty');
         myLogger.error('"%o', {
           title_selector,
-          jobsdb_job_url
-        })
+          url: jobPage.url(),
+        });
+        output = { ...output, error: error.message };
       }
-
-      return output
+      return output;
     }
 
     var output = { state: 'INIT', extracted: {}, debug: {}, error: {} };
@@ -61,7 +64,7 @@ router.post('/', async (req, res) => {
         // NOTE: input validation, may be set a schema here ?
         const req_body = req.body;
 
-        // TODO: remove me 
+        // TODO: remove me
         // const { url } = req_body;
 
         myLogger.info('%o', { jobsdb_job_url });
@@ -87,28 +90,9 @@ router.post('/', async (req, res) => {
         myLogger.info('browser started');
 
         const jobPage = page;
-        var title_selector = 'div[data-automation="detailsTitle"] h1'
-        var { result } = await getFromEvaluate(jobPage, title_selector)
-        var jobTitle = result
-
-        // const o_jobTitle = await jobPage.evaluate((selector) => {
-        //   var output = {result: '',error:{}}
-        //   try {
-        //     const title = document.querySelector(selector).textContent;
-        //     output = {...output, result: title}
-        //   } catch (error) {
-        //     output = {...output, error: error.message}
-        //   }
-        //   return output;
-        // },title_selector);
-
-        // var jobTitle = ''
-        // if (o_jobTitle.result != '' ) {
-        //   jobTitle = o_jobTitle.result
-        // }else{
-        //   myLogger.error('jobTitle error logged')
-        //   myLogger.error('%o', o_jobTitle)
-        // }
+        var title_selector = 'div[data-automation="detailsTitle"] h1';
+        var { result } = await getFromEvaluateTextContent(jobPage, title_selector);
+        var jobTitle = result;
 
         const _jobDetailsHeaderRawHTML = await jobPage.evaluate(() => {
           const title = document.querySelector('div[data-automation="jobDetailsHeader"]').outerHTML;
@@ -194,10 +178,9 @@ router.post('/', async (req, res) => {
     // NOTE: receiver -> src/flow-handler/src/routes/jobsdb_link_extract_cb.js
     await postResult(callback_url, output.extracted);
   } catch (error) {
-
-    myLogger.error(error.message)
-    myLogger.error('req.body %o', req.body)
-    myLogger.error('error %o', error)
+    myLogger.error(error.message);
+    myLogger.error('req.body %o', req.body);
+    myLogger.error('error %o', error);
   }
 });
 
