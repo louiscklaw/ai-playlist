c17edbbe6a26a77623ddf22963df970b7e57ad24
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-22 18:42:30 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/routes/jobsdbPostExtract.js
+++ poe-tryout/docker-poe-tryout/src/jobsdb-link-extractor/src/routes/jobsdbPostExtract.js
@@ -31,148 +31,154 @@ router.post('/', async (req, res) => {
     const { error } = schema.validate(req.body);
     if (error) throw new Error('input json is invalid');
 
-    const { jobsdb_job_url, callback_url } = req.body;
-    if (jobsdb_job_url.trim().search(/https:\/\/hk.jobsdb.com\/?$/) > -1) throw new Error('LINK_CONTAIN_NO_POST');
+    try {
+      const { jobsdb_job_url, callback_url } = req.body;
+      if (jobsdb_job_url.trim().search(/https:\/\/hk.jobsdb.com\/?$/) > -1) throw new Error('LINK_CONTAIN_NO_POST');
 
-    res.send({ state: 'scheduled' });
+      res.send({ state: 'scheduled' });
 
-    var output = { state: 'INIT', extracted: {}, debug: {}, error: {} };
-    var browser = {},
-      page = {};
+      var output = { state: 'INIT', extracted: {}, debug: {}, error: {} };
+      var browser = {},
+        page = {};
 
-    var retry = 3;
-    var done = false;
+      var retry = 3;
+      var done = false;
 
-    for (var i = 0; i < retry; i++) {
-      try {
-        // NOTE: input validation, may be set a schema here ?
-        const req_body = req.body;
+      for (var i = 0; i < retry; i++) {
+        try {
+          // NOTE: input validation, may be set a schema here ?
+          const req_body = req.body;
 
-        // TODO: remove me
-        // const { url } = req_body;
+          // TODO: remove me
+          // const { url } = req_body;
 
-        myLogger.info('%o', { jobsdb_job_url });
+          myLogger.info('%o', { jobsdb_job_url });
 
-        if (!validUrl.isUri(jobsdb_job_url)) throw new Error(`invalid url ${jobsdb_job_url}`);
+          if (!validUrl.isUri(jobsdb_job_url)) throw new Error(`invalid url ${jobsdb_job_url}`);
 
-        const post_id = jobsdb_job_url.replace('.html', '').split('-').pop();
-        if (!post_id) throw new Error('post_id is required');
+          const post_id = jobsdb_job_url.replace('.html', '').split('-').pop();
+          if (!post_id) throw new Error('post_id is required');
 
-        // const url = `https://hk.jobsdb.com/hk/en/job/validation-assistant-100003010509868`;
+          // const url = `https://hk.jobsdb.com/hk/en/job/validation-assistant-100003010509868`;
 
-        // NOTE: considerate the test, split the calculation to call input
-        // const post_id = url.split('-').pop();
+          // NOTE: considerate the test, split the calculation to call input
+          // const post_id = url.split('-').pop();
 
-        // slowMo: 1,
-        browser = await puppeteer.connect({
-          browserWSEndpoint: `ws://${BROWSERLESS_HOST}:3000`,
-          defaultViewport: { width: 1920, height: 1080 * 3 },
-        });
-        page = await browser.newPage();
-        await page.goto(jobsdb_job_url, { waitUntil: 'networkidle2' });
-
-        myLogger.info('browser started');
-
-        const jobPage = page;
-        var title_selector = 'div[data-automation="detailsTitle"] h1';
-        var { result } = await getFromEvaluateTextContent(jobPage, title_selector);
-        var jobTitle = result;
-
-        const _jobDetailsHeaderRawHTML = await jobPage.evaluate(() => {
-          const title = document.querySelector('div[data-automation="jobDetailsHeader"]').outerHTML;
-          return title;
-        });
-
-        const { companyName, jobAddress, postDate, _debugList } = await jobPage.evaluate(() => {
-          var output = {};
-          var debugList = [];
-          document.querySelectorAll('div[data-automation="jobDetailsHeader"] span').forEach((ele, idx) => {
-            if (idx == 1) output['companyName'] = ele.textContent || '';
-            if (idx == 2) output['jobAddress'] = ele.textContent || '';
-            if (idx == 3) output['postDate'] = ele.textContent || '';
-            debugList.push(ele.innerHTML);
+          // slowMo: 1,
+          browser = await puppeteer.connect({
+            browserWSEndpoint: `ws://${BROWSERLESS_HOST}:3000`,
+            defaultViewport: { width: 1920, height: 1080 * 3 },
           });
-          output['_debugList'] = debugList;
-          return output;
-        });
+          page = await browser.newPage();
+          await page.goto(jobsdb_job_url, { waitUntil: 'networkidle2' });
 
-        const jobHighlight = await jobPage.evaluate(() => {
-          const title = document.querySelector(
-            'div[data-automation="job-details-job-highlights"] > div:nth-child(1) > div:nth-child(2)',
-          ).textContent;
-          return title;
-        });
-        var { description, _jobDescriptionRaw, __jobDescriptionRawProcessed } = await jobPage.evaluate(() => {
-          const _jobDescriptionRaw = document.querySelector('div[data-automation="jobDescription"]').outerHTML;
+          myLogger.info('browser started');
 
-          var __jobDescriptionRawProcessed = _jobDescriptionRaw.replace(
-            /<\/[p|h1|h2|b]>/g,
-            '{{this_should_be_a_newline}}</p>',
-          );
-          var html = document.createElement('html');
-          html.innerHTML = __jobDescriptionRawProcessed;
-          const description = html.textContent;
+          const jobPage = page;
+          var title_selector = 'div[data-automation="detailsTitle"] h1';
+          var { result } = await getFromEvaluateTextContent(jobPage, title_selector);
+          var jobTitle = result;
 
-          return { description, _jobDescriptionRaw, __jobDescriptionRawProcessed };
-        });
-        // NOTE: string_cleaning_test.js
-        var jobDescription = description;
-        jobDescription = jobDescription.replace(/{{this_should_be_a_newline}}/g, '\n');
-        jobDescription = jobDescription.replace(/\n +/g, '\n');
-        jobDescription = jobDescription.replace(/\n+/g, '\n');
-        jobDescription = jobDescription.replace(/ /g, ' ');
+          const _jobDetailsHeaderRawHTML = await jobPage.evaluate(() => {
+            const title = document.querySelector('div[data-automation="jobDetailsHeader"]').outerHTML;
+            return title;
+          });
 
-        var _jobDescriptionMd = htmlToMarkdown(_jobDescriptionRaw);
-        fs.writeFileSync('/share/screenshot/hello-html.html', _jobDescriptionRaw, { encoding: 'utf8' });
-        fs.writeFileSync('/share/screenshot/hello-md.md', _jobDescriptionMd, { encoding: 'utf8' });
+          const { companyName, jobAddress, postDate, _debugList } = await jobPage.evaluate(() => {
+            var output = {};
+            var debugList = [];
+            document.querySelectorAll('div[data-automation="jobDetailsHeader"] span').forEach((ele, idx) => {
+              if (idx == 1) output['companyName'] = ele.textContent || '';
+              if (idx == 2) output['jobAddress'] = ele.textContent || '';
+              if (idx == 3) output['postDate'] = ele.textContent || '';
+              debugList.push(ele.innerHTML);
+            });
+            output['_debugList'] = debugList;
+            return output;
+          });
 
-        var screenshot_path = `${SCREENSHOT_ROOT}/jobsdb_${post_id}.png`;
-        await jobPage.screenshot({ path: screenshot_path, fullPage: true });
+          const jobHighlight = await jobPage.evaluate(() => {
+            const title = document.querySelector(
+              'div[data-automation="job-details-job-highlights"] > div:nth-child(1) > div:nth-child(2)',
+            ).textContent;
+            return title;
+          });
+          var { description, _jobDescriptionRaw, __jobDescriptionRawProcessed } = await jobPage.evaluate(() => {
+            const _jobDescriptionRaw = document.querySelector('div[data-automation="jobDescription"]').outerHTML;
 
-        var extracted = {
-          jobsdb_job_url,
-          post_id,
-          __jobDescriptionRawProcessed,
-          _debugList,
-          _jobDescriptionMd,
-          _jobDescriptionRaw,
-          _jobDetailsHeaderRawHTML,
-          companyName,
-          jobAddress,
-          jobDescription,
-          jobHighlight,
-          jobTitle,
-          postDate,
-        };
+            var __jobDescriptionRawProcessed = _jobDescriptionRaw.replace(
+              /<\/[p|h1|h2|b]>/g,
+              '{{this_should_be_a_newline}}</p>',
+            );
+            var html = document.createElement('html');
+            html.innerHTML = __jobDescriptionRawProcessed;
+            const description = html.textContent;
 
-        output = { ...output, state: 'EXTRACT_DONE', extracted };
+            return { description, _jobDescriptionRaw, __jobDescriptionRawProcessed };
+          });
+          // NOTE: string_cleaning_test.js
+          var jobDescription = description;
+          jobDescription = jobDescription.replace(/{{this_should_be_a_newline}}/g, '\n');
+          jobDescription = jobDescription.replace(/\n +/g, '\n');
+          jobDescription = jobDescription.replace(/\n+/g, '\n');
+          jobDescription = jobDescription.replace(/ /g, ' ');
 
-        if (jobTitle) {
-          done = true;
-        } else {
-          // NOTE: sometime job title is undefined even success
+          var _jobDescriptionMd = htmlToMarkdown(_jobDescriptionRaw);
+          fs.writeFileSync('/share/screenshot/hello-html.html', _jobDescriptionRaw, { encoding: 'utf8' });
+          fs.writeFileSync('/share/screenshot/hello-md.md', _jobDescriptionMd, { encoding: 'utf8' });
+
+          var screenshot_path = `${SCREENSHOT_ROOT}/jobsdb_${post_id}.png`;
+          await jobPage.screenshot({ path: screenshot_path, fullPage: true });
+
+          var extracted = {
+            jobsdb_job_url,
+            post_id,
+            __jobDescriptionRawProcessed,
+            _debugList,
+            _jobDescriptionMd,
+            _jobDescriptionRaw,
+            _jobDetailsHeaderRawHTML,
+            companyName,
+            jobAddress,
+            jobDescription,
+            jobHighlight,
+            jobTitle,
+            postDate,
+          };
+
+          output = { ...output, state: 'EXTRACT_DONE', extracted };
+
+          if (jobTitle) {
+            done = true;
+          } else {
+            // NOTE: sometime job title is undefined even success
+          }
+        } catch (error) {
+          myLogger.error('%o', error);
+          output = { ...output, state: 'EXTRACTION_ERROR', error };
         }
-      } catch (error) {
-        myLogger.error('%o', error);
-        output = { ...output, state: 'EXTRACTION_ERROR', error };
+
+        if (done) {
+          myLogger.info(`${jobsdb_job_url} done exiting...`);
+
+          break;
+        }
       }
 
-      if (done) {
-        myLogger.info(`${jobsdb_job_url} done exiting...`);
-
-        break;
-      }
+      // NOTE: receiver -> src/flow-handler/src/routes/jobsdb_link_extract_cb.js
+      await postResult(callback_url, output.extracted);
+    } catch (error) {
+      myLogger.error(error.message);
+      throw error;
     }
-
-    // NOTE: receiver -> src/flow-handler/src/routes/jobsdb_link_extract_cb.js
-    await postResult(callback_url, output.extracted);
   } catch (error) {
     if (error.message == 'LINK_CONTAIN_NO_POST') {
       myLogger.warn(`link contain no post, skipping`);
     } else {
       myLogger.error(error.message);
-      myLogger.error('req.body %o', req.body);
-      myLogger.error('error %o', error);
+      // myLogger.error('req.body %o', req.body);
+      // myLogger.error('%o', error);
+      throw error;
     }
   }
 });
