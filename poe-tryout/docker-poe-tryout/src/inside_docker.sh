dbd035caf1145b0a055cf7ed2f6af1411e003eb6
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-23 12:48:08 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/inside_docker.sh
+++ poe-tryout/docker-poe-tryout/src/inside_docker.sh
@@ -5,3 +5,8 @@ set -ex
 pushd /src/changedetect/diff-handler/src/tests/jobsdb_diff_handler/test3
   node helloworld.js
 popd
+
+# src/jobsdb-link-extractor/src/tests/jobsdbPostExtract/test2/index.js
+# pushd /src/jobsdb-link-extractor/src/tests/jobsdbPostExtract/test2
+#   node index.js
+# popd
