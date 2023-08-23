3c22c599995d9c7a92fe204a27cfab33c4106f8b
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-23 10:27:57 +0800

    update log config,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
-docker compose -f ./docker-compose.gen.yml \
+# docker logs --since 30s
+
+docker compose --since 180s -f ./docker-compose.gen.yml \
   logs -f  api-debug dbapi diff-handler flow-handler jobsdb-link-extractor jobsdb-scraper page-handler poe-scheduler-api
