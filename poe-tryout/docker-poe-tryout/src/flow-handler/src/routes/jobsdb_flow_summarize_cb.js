6622a5012f2bafaf4749e497e89d3e61b0b19667
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-23 14:00:13 +0800

    update to handke working_dir,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/routes/jobsdb_flow_summarize_cb.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/routes/jobsdb_flow_summarize_cb.js
@@ -3,16 +3,24 @@ const { jobsdbPoeSummarizeCbMachine } = require('../state_machine/jobsdb/jobsdbM
 // const { mySleep } = require('../utils/mySleep');
 const express = require('express');
 const { storeJson } = require('../utils/storeJson');
+const { myLogger } = require('../utils/myLogger');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   var output = { state: 'INIT', debug: {}, error: {} };
   var req_body = req.body;
   output = { ...output, debug: { input: { ...req_body } } };
+
   var { working_dir } = req.body;
+  myLogger.error(working_dir)
+  if (!working_dir) {
+    myLogger.warn('self testing ? working_dir undefined')
+    working_dir = `/share/testing`
+    myLogger.warn(`fallback to default working_dir ${working_dir}`)
+  }
 
   try {
-    console.log('receive callback from poe summarize ');
+    myLogger.info('receive callback from poe summarize ');
     // output.state = 'start';
     output = { ...output, state: 'start', debug: req_body };
 
@@ -27,7 +35,7 @@ router.post('/', async (req, res) => {
     // output.state = 'success';
     output = { ...output, state: 'success' };
   } catch (error) {
-    console.log({ error });
+    myLogger.error("%o",{ error });
     // output.state = 'error';
     // output.error = error;
 
