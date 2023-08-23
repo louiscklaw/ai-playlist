1bece6e34701d7a4e16f80f049fee53a848b194f
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-23 14:01:21 +0800

    update test,

--- poe-tryout/docker-poe-tryout/src/inside_docker.sh
+++ poe-tryout/docker-poe-tryout/src/inside_docker.sh
@@ -6,7 +6,16 @@ pushd /src/changedetect/diff-handler/src/tests/jobsdb_diff_handler/test3
   node helloworld.js
 popd
 
+# src/flow-handler/src/tests/jobsdb_flow_summarize/index.js
+# pushd /src/flow-handler/src/tests/jobsdb_flow_summarize
+#   node index.js
+# popd
+
+
 # src/jobsdb-link-extractor/src/tests/jobsdbPostExtract/test2/index.js
+# pushd /src/jobsdb-link-extractor/src/tests/jobsdbPostExtract/test2
+  # node index.js
+# popd
 # pushd /src/jobsdb-link-extractor/src/tests/jobsdbPostExtract/test2
 #   node index.js
 # popd
