a4e441406d4b309825c09e37cbb369c5b9ce0771
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-24 14:42:53 +0800

    update telegramSendMessage,

--- poe-tryout/docker-poe-tryout/src/page-handler/src/index.js
+++ poe-tryout/docker-poe-tryout/src/page-handler/src/index.js
@@ -15,6 +15,7 @@ try {
   // app.use('/post_helloworld', require('./routes/post_helloworld'));
   app.use('/post-telegram-message', require('./routes/postTelegramMessage'));
   app.use('/telegram-send-message', require('./routes/telegramSendMessage'));
+  app.use('/telegram-send-alert', require('./routes/telegramSendAlert'));
   app.use('/hw-telegram', require('./routes/hwTelegram'));
   app.use('/helloworld', require('./routes/helloworld'));
 
--- poe-tryout/docker-poe-tryout/src/page-handler/src/routes/postTelegramMessage.js
+++ poe-tryout/docker-poe-tryout/src/page-handler/src/routes/postTelegramMessage.js
@@ -1,16 +1,11 @@
-const TG = require('telegram-bot-api');
-
 const express = require('express');
 const router = express.Router();
 
-const { TELEGRAM_TOKEN, TELEGRAM_CHAT_ID } = process.env;
-
-const api = new TG({
-  token: TELEGRAM_TOKEN,
-});
+const { tgSendMarkDownMessage } = require('../utils/tgApi');
 
 const SEND_MESSAGE_FAILED = 'SEND_MESSAGE_FAILED';
 const SEND_MESSAGE_DONE = 'SEND_MESSAGE_DONE';
+const SEND_MESSAGE_INIT = 'SEND_MESSAGE_INIT';
 
 // https://core.telegram.org/bots/api#available-methods
 
@@ -23,25 +18,20 @@ function saintizeText(in_text) {
 }
 
 router.post('/', async (req, res) => {
-  var output = { state: 'init', debug: req.body, error: {} };
+  var output = { state: SEND_MESSAGE_INIT, debug: req.body, error: '' };
 
   try {
     var { text } = req.body;
     var saintized_text = saintizeText(text);
 
-    // var result = await api.getMe()
-    await api.sendMessage({
-      chat_id: TELEGRAM_CHAT_ID,
-      text: saintized_text,
-      parse_mode: 'MarkdownV2',
-    });
+    await tgSendMarkDownMessage(saintized_text);
 
-    output = { ...output, state: SEND_MESSAGE_DONE, debug: req.body };
+    output = { ...output, state: SEND_MESSAGE_DONE };
   } catch (error) {
-    console.log(error);
-    output = { ...output, state: SEND_MESSAGE_FAILED, error: error.message };
-    
-    console.log({output})
+    output = { ...output, state: SEND_MESSAGE_FAILED, error: JSON.stringify(error) };
+    myLogger.error('%o', { output });
+
+    storeErrorJson(output, '/logs/error/page-handler');
   }
 
   res.send(output);
