548a73baca98c2ba39aa7815112bcdde43df5515
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-26 02:01:29 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/state_machine/jobsdb/onExtractDone/index.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/state_machine/jobsdb/onExtractDone/index.js
@@ -1,14 +1,16 @@
-const fetch = require('node-fetch');
-// const Joi = require('joi');
-// const fs = require('fs');
-
+const fs = require('fs');
+const ERROR_LOG_DIR = '/logs/error/onExtractDone';
 const { storeJson } = require('../../../utils/storeJson');
 const { myLogger } = require('../../../utils/myLogger');
-const { calculateMD5 } = require('../../../utils/calculateMD5');
 const { inputSchema } = require('./inputSchema');
+const ExtractPrompts = require('./ExtractPrompts');
+const { postToSummarize } = require('./postToSummarize');
+const { calculateMD5 } = require('../../../utils/calculateMD5');
+const { createDirIfNotExists } = require('../../../utils/createDirIfNotExists');
 
-const summarize_url = 'http://flow-handler:3000/jobsdb_flow_summarize';
-const SAMPLE_PREPROMPTS = ['Forget everything and start a new talk.'];
+const summarize_cb_url = 'http://flow-handler:3000/jobsdb_flow_summarize_cb';
+
+// const SAMPLE_PREPROMPTS = ['Forget everything and start a new talk.'];
 const JOB_TITLE_UNDEFINED = 'JOB_TITLE_UNDEFINED';
 // const INPUT_FROM_CONTEXT_IS_NOT_VALID = 'INPUT_FROM_CONTEXT_IS_NOT_VALID';
 
@@ -23,47 +25,39 @@ module.exports = {
 
         inputSchema(this.context);
 
-        var input_to_summarize = {
-          working_dir,
-          preprompts: SAMPLE_PREPROMPTS,
-          question_list: [
-            `
-I will pass you a job advertisement, 
-please try to summarize it in around 200 words.
-`.trim(),
-            `
-company name: ${companyName}
-job title: ${jobTitle}
-job addess: ${jobAddress}
-post date: ${postDate}
-job highlight: ${jobHighlight}
-
-job description (markdown):
-\`\`\`
-${_jobDescriptionMd}
-\`\`\`
-`.trim(),
-          ],
-          callback_url: 'http://flow-handler:3000/jobsdb_flow_summarize_cb',
-        };
+        const prompts = new ExtractPrompts(
+          companyName,
+          jobTitle,
+          jobAddress,
+          postDate,
+          jobHighlight,
+          _jobDescriptionMd,
+        );
 
         await storeJson(`${working_dir}/input_to_summarize.json`, input_to_summarize);
-
-        // // proceed to summarize
-        await fetch(summarize_url, {
-          method: 'post',
-          body: JSON.stringify(input_to_summarize),
-          headers: { 'Content-Type': 'application/json' },
+        await postToSummarize({
+          working_dir,
+          preprompts: prompts.getSamplePreprompts(),
+          question_list: prompts.getSampleQuestions(),
+          callback_url: summarize_cb_url,
         });
 
         res();
       } catch (error) {
         myLogger.error('error during processing Extract callback ...');
+
         if (error.message == JOB_TITLE_UNDEFINED) {
           myLogger.error('error as jobtitle is undefined, skipping process');
         } else {
+          await createDirIfNotExists(ERROR_LOG_DIR);
+
+          var filename = `${ERROR_LOG_DIR}/${calculateMD5(error)}.json`;
+          var payload = this.context;
+
+          fs.writeFileSync(filename, JSON.stringify({ payload, error }), { encoding: 'utf8' });
           myLogger.error('%o', error);
         }
+
         rej();
       }
     });
---
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/state_machine/jobsdb/onExtractDone/postToSummarize.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+// const Joi = require('joi');
+// const fs = require('fs');
+
+const { storeJson } = require('../../../utils/storeJson');
+const { myLogger } = require('../../../utils/myLogger');
+const { calculateMD5 } = require('../../../utils/calculateMD5');
+const { inputSchema } = require('./inputSchema');
+const ExtractPrompts = require('./ExtractPrompts');
+
+const summarize_url = 'http://flow-handler:3000/jobsdb_flow_summarize';
+
+// const SAMPLE_PREPROMPTS = ['Forget everything and start a new talk.'];
+const JOB_TITLE_UNDEFINED = 'JOB_TITLE_UNDEFINED';
+// const INPUT_FROM_CONTEXT_IS_NOT_VALID = 'INPUT_FROM_CONTEXT_IS_NOT_VALID';
+
+async function postToSummarize(input_to_summarize) {
+  await fetch(summarize_url, {
+    method: 'post',
+    body: JSON.stringify(input_to_summarize),
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+module.exports = { postToSummarize };
