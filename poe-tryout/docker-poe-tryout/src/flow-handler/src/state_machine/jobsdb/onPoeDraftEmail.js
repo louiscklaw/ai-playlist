14125d16005250deff561924246a259cf4683ad0
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-26 10:55:56 +0800

    update refactor onPoeDraftEmailDone,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/state_machine/jobsdb/onPoeDraftEmail.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/state_machine/jobsdb/onPoeDraftEmail.js
@@ -6,36 +6,27 @@ const { askPoePrepromptQuestion } = require('../../fetch/askPoePrepromptQuestion
 
 const { myLogger } = require('../../utils/myLogger');
 
-module.exports = {
-  onPoeDraftEmail: function (hello) {
-    return new Promise(async (res, rej) => {
-      try {
-        console.log('I DraftEmail');
-        const { req_body } = this.context;
-        var payload = req_body;
 
-        // var working_dir = '/share/helloworld'
-        // var summary_result_json_filename = 'summary_result.json'
-        // var summary_result = await fs.readFileSync()
+function onPoeDraftEmail(hello) {
+  return new Promise(async (res, rej) => {
+    try {
+      console.log('I DraftEmail');
+      const { req_body } = this.context;
+      var payload = req_body;
 
-        var result = await askPoePrepromptQuestion(payload);
-        // res_json = await result.json();
+      // var working_dir = '/share/helloworld'
+      // var summary_result_json_filename = 'summary_result.json'
+      // var summary_result = await fs.readFileSync()
 
-        res();
-      } catch (error) {
-        myLogger.error('error during draft email');
-        console.log(error);
-      }
-    });
-  },
-  // onPoeDraftEmailDone: function () {
-  //   return new Promise(async (res, rej) => {
-  //     // http://flow-handler:3000/jobsdb_draft_email_cb
-  //     myLogger.log('info', { message: 'I DraftEmailDone' });
-  //     // var result = await getHelloworld();
-  //     // var res_text = await result.text();
-  //     // console.log(res_text);
-  //     res();
-  //   });
-  // },
-};
+      var result = await askPoePrepromptQuestion(payload);
+      // res_json = await result.json();
+
+      res();
+    } catch (error) {
+      myLogger.error('error during draft email');
+      console.log(error);
+    }
+  });
+}
+
+module.exports = { onPoeDraftEmail }
