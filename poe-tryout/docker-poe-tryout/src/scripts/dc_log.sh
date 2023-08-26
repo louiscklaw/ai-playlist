821b5fd26109afe8a7c321dfd44d2ac35f317dae
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-26 21:48:20 +0800

    slim down scripts,

--- poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
+++ poe-tryout/docker-poe-tryout/src/scripts/dc_log.sh
@@ -5,4 +5,11 @@ set -ex
 # docker logs --since 30s
 
 docker compose -f ./docker-compose.gen.yml \
-  logs -f bait api-debug dbapi diff-handler flow-handler jobsdb-link-extractor jobsdb-scraper page-handler poe-scheduler-api openbox-poe-seat1
+  logs -f bait \
+  api-debug \
+  dbapi \
+  diff-handler \
+  flow-handler \
+  jobsdb-link-extractor \
+  jobsdb-scraper \
+  page-handler poe-scheduler-api openbox-poe-seat1
