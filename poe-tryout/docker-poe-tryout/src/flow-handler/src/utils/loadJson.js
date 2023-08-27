34f51394f1943fe6fc4b28f7ee10b6244e5daf2e
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-28 04:44:40 +0800

    update comment,

--- poe-tryout/docker-poe-tryout/src/flow-handler/src/utils/loadJson.js
+++ poe-tryout/docker-poe-tryout/src/flow-handler/src/utils/loadJson.js
@@ -5,7 +5,7 @@ const { myLogger } = require('./myLogger');
 async function loadJson(filepath) {
   var output = {};
   try {
-    myLogger.info(`writing json file to ${filepath}`);
+    myLogger.info(`loading json file from ${filepath}`);
 
     var temp_raw = await fs.readFileSync(filepath, {
       encoding: 'utf-8',
