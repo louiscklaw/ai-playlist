43b865cbd4dce1e3746bcd0425a6a6191518309e
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-28 04:44:30 +0800

    update make dir recursive,

--- poe-tryout/docker-poe-tryout/src/poe-scheduler-api/utils/createDirIfNotExists.js
+++ poe-tryout/docker-poe-tryout/src/poe-scheduler-api/utils/createDirIfNotExists.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 async function createDirIfNotExists(chatgpt_output_filename) {
   try {
-    await fs.mkdirSync(chatgpt_output_filename);
+    await fs.mkdirSync(chatgpt_output_filename, {recursive: true});
   } catch (error) {
     myLogger.warn(`${chatgpt_output_filename} already exists`);
   }
