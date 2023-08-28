00a33aa5eabe8c4ef42199a335e69687015a7c9f
Louis Law <louiscklaw@users.noreply.github.com>
2023-08-28 14:58:54 +0800

    update,

--- poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/utils/filterAlreadySeenLink.js
+++ poe-tryout/docker-poe-tryout/src/changedetect/diff-handler/src/utils/filterAlreadySeenLink.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const fs = require('fs'),
+  path = require('path');
+const ERROR_LOG_DIR = `/logs/error/${path.basename(__filename).replace('.js', '')}`;
+
 const { postJobsdbLinkExtract } = require('../utils/postJobsdbLinkExtract');
 const { createClient } = require('redis');
 const express = require('express');
@@ -9,6 +12,8 @@ const { getAddedLink } = require('../utils/getAddedLink');
 
 const { FLOW_HANDLER_ENDPOINT } = require('../config');
 const { isNewLink } = require('../utils/isNewLink');
+const { createDirIfNotExists } = require('./createDirIfNotExists');
+const { calculateMD5 } = require('./calculateMD5');
 if (!FLOW_HANDLER_ENDPOINT) throw new Error('FLOW_HANDLER_ENDPOINT is not configured');
 
 const client = createClient({
@@ -19,18 +24,31 @@ client.on('error', err => console.log('Redis Client Error', err));
 client.connect();
 
 async function filterAlreadySeenLink(links, client) {
-  var output = [];
+  var new_links = [];
+  var output = { state: 'init', debug: links, error: '' };
 
-  for (var i = 0; i < links.length; i++) {
-    var x = links[i].toString();
-    if (await isNewLink(x, client)) {
-      output.push(x);
-    } else {
-      console.log(`already seen, skipping ${x}`);
+  try {
+    output = {...output, state:'start'}
+    for (var i = 0; i < links.length; i++) {
+      var x = links[i].toString();
+      if (await isNewLink(x, client)) {
+        new_links.push(x);
+      } else {
+        myLogger.info(`already seen, skipping ${x}`);
+      }
     }
+  } catch (error) {
+    output = { ...output, state: 'error', new_links, error: JSON.stringify(error) };
+
+    await createDirIfNotExists(ERROR_LOG_DIR);
+
+    var filename = `${ERROR_LOG_DIR}/${calculateMD5(error)}.json`;
+    fs.writeFileSync(filename, JSON.stringify(output), { encoding: 'utf8' });
+
+    myLogger.error(JSON.stringify(error));
   }
 
-  return output;
+  return new_links;
 }
 
-module.exports ={filterAlreadySeenLink}
+module.exports = { filterAlreadySeenLink };
