0f55c535f79ccd8080e638e226fc3e0290f7eacf
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-04 18:05:21 +0800

    update comment unused code,

--- poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/routes/chatGPT/index.js
+++ poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/routes/chatGPT/index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const NO_QUESTION_FOUND = 'no question found';
-const QUESTION_LIST_NOT_FOUND = 'question list not found';
+// TODO: remove me
+// const NO_QUESTION_FOUND = 'no question found';
+// const QUESTION_LIST_NOT_FOUND = 'question list not found';
 
-const puppeteer = require('puppeteer-extra');
+// const puppeteer = require('puppeteer-extra');
 
-const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+// const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+// puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
+// const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+// puppeteer.use(StealthPlugin());
 
 // require('dotenv').config();
-
-const { SRC_ROOT, UTILS_ROOT, WORKER_ROOT } = require('../../config');
-const { myLogger } = require('../../utils/myLogger');
-const { checkInput } = require('./checkInput');
-const { reportOffline } = require('../../utils/reportPoeSeatOffline');
-const { DONE, ERROR } = require('../../constants');
-const { ASK_INIT, ASK_DONE } = require(`${SRC_ROOT}/constants`);
-const { chatGPTSolver, testLanding } = require(`${WORKER_ROOT}/poe/chatGPT`);
+// const { SRC_ROOT, UTILS_ROOT, WORKER_ROOT } = require('../../config');
+// const { myLogger } = require('../../utils/myLogger');
+// const { checkInput } = require('./checkInput');
+// const { reportOffline } = require('../../utils/reportPoeSeatOffline');
+// const { DONE, ERROR } = require('../../constants');
+// const { ASK_INIT, ASK_DONE } = require(`${SRC_ROOT}/constants`);
+// const { chatGPTSolver, testLanding } = require(`${WORKER_ROOT}/poe/chatGPT`);
 
 require('./helloworld')(router);
 require('./testLanding')(router);
