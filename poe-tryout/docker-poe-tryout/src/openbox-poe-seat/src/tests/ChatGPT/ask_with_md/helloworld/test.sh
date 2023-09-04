98ce2fd2045263f867242e650ec884a2f386c1cf
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-04 22:46:36 +0800

    update test,

--- poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/tests/ChatGPT/ask_with_md/helloworld/test.sh
+++ poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/tests/ChatGPT/ask_with_md/helloworld/test.sh
@@ -4,8 +4,7 @@ set -ex
 
 curl -X POST http://openbox-poe-seat1:3000/chatGPT/ask_with_md \
    -H 'Content-Type: application/json' \
-   -d @./input.json 
-   # \
-   # -o result.json
+   -d @./input.json \
+   -o result.json
 
 # curl http://openbox-poe-seat1:3000/chatGPT/helloworld
