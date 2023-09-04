2b8cce6b6896aa6fb3264beb2977515d33f5546f
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-04 17:59:11 +0800

    refactor ask route,

--- poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/routes/chatGPT/index.js
+++ poe-tryout/docker-poe-tryout/src/openbox-poe-seat/src/routes/chatGPT/index.js
@@ -24,45 +24,6 @@ const { chatGPTSolver, testLanding } = require(`${WORKER_ROOT}/poe/chatGPT`);
 
 require('./helloworld')(router);
 require('./testLanding')(router);
-
-router.post('/ask', async (req, res) => {
-  var json_input = req.body;
-  var output = {
-    state: ASK_INIT,
-    input: json_input,
-    error: '',
-    chat_history: { q_and_a: { preprompts: [], history: [] } },
-  };
-
-  try {
-    console.log(json_input)
-    
-    checkInput(json_input);
-
-    var { question_list, preprompts } = json_input;
-    // res.send(question_list)
-    // TODO: check using schema
-    if (!question_list) throw new Error(QUESTION_LIST_NOT_FOUND);
-    if (question_list?.length < 1) throw new Error(NO_QUESTION_FOUND);
-    // NOTE: question list valid after this line
-
-    var temp_history = await chatGPTSolver(question_list, preprompts);
-    var { state, preprompts, history } = temp_history;
-    if (state != 'done') throw new Error('error during ask ChatGPT');
-
-    output = {
-      ...output,
-      state: ASK_DONE,
-      json_input,
-      chat_history: { q_and_a: { preprompts, history } },
-    };
-  } catch (error) {
-    myLogger.error(JSON.stringify(error));
-    output = { ...output, state: ERROR, error: JSON.stringify(error) };
-    reportOffline();
-  }
-
-  res.send(output);
-});
+require('./ask')(router);
 
 module.exports = router;
