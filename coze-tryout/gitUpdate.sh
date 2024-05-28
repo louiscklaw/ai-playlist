70ec221f69dc21089323cfbcc21e81552762db6f
louiscklaw <louiscklaw@user.noreply.github.com>
2024-05-28 14:05:20 +0800

    update coze-tryout,

---
+++ coze-tryout/gitUpdate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+git add . 
+git commit -m"update coze-tryout,"
+
+git push
+
+echo "done"
--- coze-tryout/proxy-tryout/dc_up.bat
+++
@@ -1,3 +0,0 @@
-docker compose build 
-docker compose up -d coze_proxy
-docker compose exec -it coze_proxy sh
